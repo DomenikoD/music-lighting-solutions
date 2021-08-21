e46f5486c8a1aa021da02acdaa37a1f1b35ef668
Domeniko <domeniko.divjak@gmail.com>
2021-08-21 20:59:44 +0000

    ico2 bug

--- config/CustomIcons.jsx
+++ config/CustomIcons.jsx
@@ -1,11 +1,9 @@
 import makeFAIcon from "utils/makeFAIcon";
 
-import { faShoppingCart, faLaptop, faLock, faLightbulb, faSun, faCartCheck } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb, faSun, faCartCheck } from "@fortawesome/free-solid-svg-icons";
 
-export const ShoppingCartIcon = makeFAIcon(faShoppingCart);
-export const LaptopIcon = makeFAIcon(faLaptop);
-export const LockIcon = makeFAIcon(faLock);
+export const ShoppingCartIcon = makeFAIcon(faSun);
+export const LaptopIcon = makeFAIcon(faCartCheck);
+export const LockIcon = makeFAIcon(faLightbulb);
 
-export const SunIcon = makeFAIcon(faSun);
-export const CartCheckIcon = makeFAIcon(faCartCheck);
-export const LightbulbIcon = makeFAIcon(faLightbulb);
+
